7e662b9a808f42b755b6ceb1cc69bd57204dcb7e
HiroIshida <spitfire.docomo@gmail.com>
2019-04-26 23:58:11 +0900

    add pickup_highest_box.py

---
+++ script/pickup_highest_box.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import Header
+from jsk_recognition_msgs.msg import BoundingBoxArray
+from jsk_recognition_msgs.msg import BoundingBox
+
+inf = 1000000000
+
+rospy.init_node("pickup_highest_box")
+pub = rospy.Publisher('/pickup_highest_box/output', BoundingBox, queue_size=1)
+
+def callback(bbox_array):
+    print "fuck"
+    z_max = -inf
+    bbox_highest = BoundingBox()
+    for bbox in bbox_array.boxes:
+        z_box = bbox.pose.position.z + bbox.dimensions.z * 0.5
+        if z_box > z_max:
+            bbox_highest = bbox
+            z_max = z_box
+    pub.publish(bbox_highest)
+
+sub = rospy.Subscriber('/plane2box/output', BoundingBoxArray, callback)
+print "node start"
+rospy.spin()
+
+
+
+
+
+
