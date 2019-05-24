ced12fa8c6d0dba0372aaaaee9c5b579e694a8de
HiroIshida <spitfire.docomo@gmail.com>
2019-05-24 18:03:26 +0900

    read image date, relative path to absolute path

--- particle_filter/main.cpp
+++ particle_filter/main.cpp
@@ -2,8 +2,8 @@
 int main()
 { 
   auto predicate = gen_hsi_filter(-100.0, 100.0, 0.3, 1.0, 0.5, 0.8);
-  cv::Mat img1 = cv::imread("image/pre.png", CV_LOAD_IMAGE_COLOR);
-  cv::Mat img2 = cv::imread("image/post_without_move.png", CV_LOAD_IMAGE_COLOR);
+  cv::Mat img1 = cv::imread("/home/h-ishida/catkin_ws/src/pr2_send_me_a_flower/particle_filter/image/pre.png", CV_LOAD_IMAGE_COLOR);
+  cv::Mat img2 = cv::imread("/home/h-ishida/catkin_ws/src/pr2_send_me_a_flower/particle_filter/image/post_without_move.png", CV_LOAD_IMAGE_COLOR);
   cv::Mat img1_filtered = convert_bf(img1, predicate);
   cv::Mat img2_filtered = convert_bf(img2, predicate);
   int cost = compute_cost(img1_filtered, img2_filtered);
