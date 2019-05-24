91f8761df0f3ca6fb808903a4ecf815e064a2ca0
HiroIshida <spitfire.docomo@gmail.com>
2019-05-24 18:49:02 +0900

    closure which store first receoived image as image_ref

--- particle_filter/tester.cpp
+++ particle_filter/tester.cpp
@@ -12,15 +12,22 @@
 //https://stackoverflow.com/questions/45340189/ros-use-lambda-as-callback-in-nodehandle-subscribe
 
 boost::function<void(const sensor_msgs::Image&)> gen_callback(){
-  cv::Mat img_ref = cv::imread("/home/h-ishida/catkin_ws/src/pr2_send_me_a_flower/particle_filter/image/pre.png", CV_LOAD_IMAGE_COLOR);
+  //cv::Mat img_ref = cv::imread("/home/h-ishida/catkin_ws/src/pr2_send_me_a_flower/particle_filter/image/pre.png", CV_LOAD_IMAGE_COLOR);
+  auto predicate = gen_hsi_filter(-100.0, 100.0, 0.3, 1.0, 0.5, 0.8);
+  cv::Mat img_filtered_referece;
+  bool isInit = true;
 
-  auto fn_callback = [=](const sensor_msgs::Image& img){
+  // closure using lambda
+  auto fn_callback = [=](const sensor_msgs::Image& img) mutable {
     cv_bridge::CvImageConstPtr cv_ptr = cv_bridge::toCvCopy(img, sensor_msgs::image_encodings::BGR8);
-    auto predicate = gen_hsi_filter(-100.0, 100.0, 0.3, 1.0, 0.5, 0.8);
-    auto img_filtered_referece = convert_bf(img_ref, predicate);
     auto img_filtered_received = convert_bf(cv_ptr->image, predicate);
-    int cost = compute_cost(img_filtered_referece, img_filtered_received);
-    std::cout<< cost <<std::endl;
+    if(isInit){
+      img_filtered_referece = img_filtered_received;
+      isInit = false;
+    }else{
+      int cost = compute_cost(img_filtered_referece, img_filtered_received);
+      std::cout<<cost<<std::endl;
+    }
   };
 
   return fn_callback;
