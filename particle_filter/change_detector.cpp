bc99240cad83fe873739dccf8d6f71ff4fcaf57b
HiroIshida <spitfire.docomo@gmail.com>
2019-05-25 06:22:10 +0900

    publish max_cost_diff

--- particle_filter/change_detector.cpp
+++ particle_filter/change_detector.cpp
@@ -14,12 +14,13 @@
 class ChangeDetector {
 
   ros::NodeHandle nh;
-  ros::Publisher pub_cost, pub_cost_diff, pub_image;
+  ros::Publisher pub_cost, pub_cost_diff, pub_max_cost_diff, pub_image;
   ros::Subscriber sub_image;
   ros::ServiceServer service_init;
 
   int cost_reference;
   int cost_pre;
+  int max_cost_diff;
   cv::Mat img_reference;
   bool isInit_ref_image;
   bool isInit_ref_cost;
@@ -36,20 +37,24 @@ private:
 ChangeDetector::ChangeDetector(){
   pub_cost = nh.advertise<std_msgs::Int64>("/cost", 1);
   pub_cost_diff = nh.advertise<std_msgs::Int64>("/cost_diff", 1);
+  pub_max_cost_diff = nh.advertise<std_msgs::Int64>("/max_cost_diff", 1);
   pub_image = nh.advertise<sensor_msgs::Image>("/debug_image", 1);
   sub_image = nh.subscribe("/kinect_head/rgb/image_raw", 1, &ChangeDetector::callback, this);
   service_init = nh.advertiseService("trigger_change_detector_init", &ChangeDetector::req_handler, this);
   isInit_ref_image = true;
   isInit_ref_cost = true;
   isInit_cost_pre = true;
+  max_cost_diff = 0;
 }
 
 bool ChangeDetector::req_handler(std_srvs::Empty::Request& req, std_srvs::Empty::Response& res)
 {
+  //TODO
   ROS_INFO("received trigger");
   isInit_ref_image = true;
   isInit_ref_cost = true;
   isInit_cost_pre = true;
+  max_cost_diff = 0;
 }
 
 void ChangeDetector::callback(const sensor_msgs::Image& msg)
@@ -78,14 +83,19 @@ void ChangeDetector::callback(const sensor_msgs::Image& msg)
       }else{
         auto img_diff = diff_image(img_reference, img_received);
         sensor_msgs::ImagePtr msg_debug = cv_bridge::CvImage(std_msgs::Header(), "bgr8", img_diff).toImageMsg();
-        std_msgs::Int64 msg_cost, msg_cost_diff;
-        msg_cost.data = abs(cost - cost_reference);
+        std_msgs::Int64 msg_cost, msg_cost_diff, msg_max_cost_diff;
 
-        int cost_diff = abs(cost - cost_pre);
+        int cost_diff = abs(cost - cost_reference);
+        max_cost_diff = (cost_diff > max_cost_diff) ? cost_diff : max_cost_diff;
+
+        msg_cost.data = cost_diff;
         msg_cost_diff.data = cost_diff;
+        msg_max_cost_diff.data = max_cost_diff;
+
         pub_image.publish(msg_debug);
         pub_cost.publish(msg_cost);
         pub_cost_diff.publish(msg_cost_diff);
+        pub_max_cost_diff.publish(msg_max_cost_diff);
 
         cost_pre = cost;
       }
